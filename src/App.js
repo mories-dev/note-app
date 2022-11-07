66eecf060dad4a14295bc686820381e463ae2e54
mories-dev <mories1223@gmail.com>
2022-11-08 00:40:13 +0900

    noteアプリ実装

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Main from './components/Main';
+import Sidebar from './components/Sidebar';
+import uuid from 'react-uuid';
 
 function App() {
+  const [notes, setNotes] = useState(JSON.parse(localStorage.getItem("notes")) || []);
+  const [activeNote, setActiveNote] = useState("");
+  useEffect(() => {
+    //ローカルストレージにノートを保存する
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes])
+
+  useEffect(() => {
+    setActiveNote(notes[0].id);
+  }, [notes])
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "新しいノート",
+      content: "",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote])
+  }
+  const onDeleteNote = (id) => {
+    const filterNotes = notes.filter((note) => note.id !== id);
+    setNotes(filterNotes);
+  }
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote);
+  }
+  const onUpdateNote = (updatedNote) => {
+    //修正された新しいノートの配列を返す
+    const updatedNotesArray = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote
+      } else {
+        return note;
+      }
+    })
+    setNotes(updatedNotesArray);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar
+        notes={notes}
+        onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+      <Main
+        activeNote={getActiveNote()}
+        onUpdateNote={onUpdateNote}
+      />
     </div>
   );
 }
